064886744b34a7288216d8bc6fcc7ca9fc18b450
EdwardOakley <47759261+fatchild@users.noreply.github.com>
2024-05-25 20:53:53 +0100

    kata 71 (fix)

--- 0071_Reverse_or_Rotate/perl/Solution.pm
+++ 0071_Reverse_or_Rotate/perl/Solution.pm
@@ -4,19 +4,41 @@ use 5.30.0;
 use strict;
 use warnings;
 use Exporter qw(import);
-our @EXPORT_OK = qw(find_nb);
+our @EXPORT_OK = qw(revrot);
 
-sub find_nb {
-    my ($m) = @_;
+sub revrot {  
+    my ($string, $size) = @_;
+
+    return "" if $size <= 0 || $string eq "";
+    return "" if length( $size ) > length( $string );
     
-    my $n = 0;
-    my $ans = 0;
-    
-    while( $ans < $m )
+    # chunks
+    # a chunk here is a substring of the initial string) of size sz
+    my @chunks = ();
+    while( length( $string ) > 0 )
     {
-        $n++;
-        $ans += $n ** 3;
-        return -1 if $ans > $m;
-        return $n if $ans == $m;
+        if( length( $string ) < $size )
+        {
+            $string = "";
+        }
+        else
+        {
+            my $chunk = substr( $string, 0, $size );
+            # If the sum of a chunk's digits is divisible by 2, reverse that chunk
+            my $sum_of_string = 0;
+            my @sum_string = split( "", $chunk );
+            for ( @sum_string ) { $sum_of_string += $_ }
+            if( $sum_of_string % 2 == 0 )
+            {
+                $chunk = reverse( @sum_string );
+            }
+            else
+            {
+                $chunk = substr( $chunk, 1, $size-1 ) . substr( $chunk, 0, 1 );
+            }
+            push @chunks, $chunk;
+            $string = substr( $string, $size, length( $string ) );
+        }
     }
+    return join( "", @chunks );
 }
--- 0071_Reverse_or_Rotate/perl/Test.pl
+++ 0071_Reverse_or_Rotate/perl/Test.pl
@@ -1,35 +1,23 @@
-# You can use `Test::More` to write tests.
-# https://metacpan.org/pod/Test::More
-# use strict;
-# use warnings;
-# use Test::More;
-# `Test::Most` can be used to load commonly needed features.
-# See https://metacpan.org/pod/Test::Most
 use strict;
 use warnings;
 use Test::More;
-use Solution qw(find_nb);
+use Solution qw(revrot);
 
-sub dotest {
-    my $k = shift;
-    my $exp = shift;
-    my $ans = find_nb($k);
-    is($ans, $exp, "find_nb($k)");
+sub testing {
+    my ($s, $sz, $exp) = @_;
+    my $ans = revrot($s, $sz);
+    is($ans, $exp, "");
 }
 
 subtest "basics" => sub {
-    dotest(1, 1);
-    dotest(2, -1);
-    dotest(3, -1);
-    dotest(4, -1);
-    dotest(9, 2);
-    dotest(36, 3);
-    dotest(100, 4);
+    testing("1234", 0, "");
+    testing("", 0, "");
+    testing("1234", 5, "");
+    my $s = "733049910872815764";
+    testing($s, 5, "330479108928157");
+    $s = "73304991087281576455176044327690580265896";
+    testing($s, 8, "1994033775182780067155464327690480265895");
 
-    dotest(4183059834009, 2022);
-    dotest(24723578342962, -1);
-    dotest(135440716410000, 4824);
-    
 };
 
 done_testing();
